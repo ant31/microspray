cc16e93e08a0d4b81e8e54dc2585edbbb5fc4538
ant31 <2t.antoine@gmail.com>
2026-02-16 17:06:35 +0100

    Update vendor

--- vendors/kubespray/scripts/component_hash_update/src/component_hash_update/components.py
+++ vendors/kubespray/scripts/component_hash_update/src/component_hash_update/components.py
@@ -116,9 +116,4 @@ infos = {
         "graphql_id": "R_kgDODQ6RZw",
         "binary": True,
     },
-    "prometheus_operator_crds": {
-        "url": "https://github.com/prometheus-operator/prometheus-operator/releases/download/v{version}/stripped-down-crds.yaml",
-        "graphql_id": "R_kgDOBBxPpw",
-        "binary": True,
-    },
 }
